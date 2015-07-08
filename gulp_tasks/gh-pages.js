d697d95670f3266df285768720249dc97f4fc391
Jan Forst <ensonador@gmail.com>
2015-07-08 23:51:16 +0200

    adding gulp-gh-pages

---
+++ gulp_tasks/gh-pages.js
@@ -0,0 +1,12 @@
+var merge = require('merge2');
+
+module.exports = function (gulp, plugins) {
+	return function () {
+		return merge([
+			gulp.src('dist/*').pipe(gulp.dest('deploy/dist')),
+			gulp.src('doc/**/*').pipe(gulp.dest('deploy/doc')),
+			gulp.src('test/images/*').pipe(gulp.dest('deploy/test/images')),
+			gulp.src('test/browser_test.html').pipe(gulp.dest('deploy/test'))
+		], gulp.src('deploy/**/*').pipe(plugins.ghPages({message: 'update', cacheDir: 'temp/deploy'})));
+	};
+};
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var plugins = require('gulp-load-plugins')();
 var sequence = require('run-sequence');
 
 function getTask(task) {
-    return require('./gulp_tasks/' + task)(gulp, plugins);
+	return require('./gulp_tasks/' + task)(gulp, plugins);
 }
 
 gulp.task('tslint', getTask('tslint'));
@@ -12,7 +12,9 @@ gulp.task('doc', getTask('typedoc'));
 gulp.task('browserify', getTask('browserify'));
 
 gulp.task('build', function (done) {
-    sequence('ts', 'browserify', done);
+	sequence('ts', 'browserify', done);
 });
 
 gulp.task('default', ['tslint', 'doc', 'build']);
+
+gulp.task('deploy', ['default'], getTask('gh-pages'));
